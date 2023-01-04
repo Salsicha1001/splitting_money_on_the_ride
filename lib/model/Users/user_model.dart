b041178dcea09b1cad2c63b208e8b3c6f1c2b20f
Salsicha1001 <felumais14@gmail.com>
2023-01-04 20:45:09 -0300

    user model

---
+++ lib/model/Users/user_model.dart
@@ -0,0 +1,10 @@
+class UserModel {
+  late String idUser;
+  late String name;
+  late String nickname;
+  late String email;
+  late String password;
+  late String photoUser;
+  UserModel({ required this.name, required this.nickname, required this.email, 
+  required this.password, required this.photoUser});
+}
